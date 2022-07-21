c384b8715732ab23c363a853d27508064b11564d
eric <ericposas@outlook.com>
2022-07-21 11:58:54 -0500

    exported dump_smashbros tables

--- dump_smashbros.sql
+++ dump_smashbros.sql
@@ -169,6 +169,192 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: abilities; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.abilities (
+    id integer NOT NULL,
+    character_id integer NOT NULL,
+    abilities text
+);
+
+
+ALTER TABLE public.abilities OWNER TO admin;
+
+--
+-- Name: abilities_character_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
+--
+
+CREATE SEQUENCE public.abilities_character_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.abilities_character_id_seq OWNER TO admin;
+
+--
+-- Name: abilities_character_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
+--
+
+ALTER SEQUENCE public.abilities_character_id_seq OWNED BY public.abilities.character_id;
+
+
+--
+-- Name: abilities_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
+--
+
+CREATE SEQUENCE public.abilities_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.abilities_id_seq OWNER TO admin;
+
+--
+-- Name: abilities_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
+--
+
+ALTER SEQUENCE public.abilities_id_seq OWNED BY public.abilities.id;
+
+
+--
+-- Name: characters; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.characters (
+    id integer NOT NULL,
+    name character varying NOT NULL
+);
+
+
+ALTER TABLE public.characters OWNER TO admin;
+
+--
+-- Name: characters_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
+--
+
+CREATE SEQUENCE public.characters_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.characters_id_seq OWNER TO admin;
+
+--
+-- Name: characters_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
+--
+
+ALTER SEQUENCE public.characters_id_seq OWNED BY public.characters.id;
+
+
+--
+-- Name: abilities id; Type: DEFAULT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.abilities ALTER COLUMN id SET DEFAULT nextval('public.abilities_id_seq'::regclass);
+
+
+--
+-- Name: abilities character_id; Type: DEFAULT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.abilities ALTER COLUMN character_id SET DEFAULT nextval('public.abilities_character_id_seq'::regclass);
+
+
+--
+-- Name: characters id; Type: DEFAULT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.characters ALTER COLUMN id SET DEFAULT nextval('public.characters_id_seq'::regclass);
+
+
+--
+-- Data for Name: abilities; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.abilities (id, character_id, abilities) FROM stdin;
+8	27	Final Cutter,Stone,Hammer,Inhale
+9	28	Blaster,Fire Fox,Reflector,Fox Illusion
+10	29	Falcon Punch, Raptor Boost, Falcon Kick, Falcon Dive
+11	30	Bullet Climax, Heel Slide / After Burner Kick, Witch Twist, Witch Time
+12	32	Charge Shot, Missile, Screw Attack, Bomb
+14	35	Shield Breaker, Dancing Blade, Dolphin Slash, Counter
+\.
+
+
+--
+-- Name: abilities_character_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
+--
+
+SELECT pg_catalog.setval('public.abilities_character_id_seq', 1, false);
+
+
+--
+-- Name: abilities_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
+--
+
+SELECT pg_catalog.setval('public.abilities_id_seq', 14, true);
+
+
+--
+-- Data for Name: characters; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.characters (id, name) FROM stdin;
+27	Kirby
+28	Fox
+29	Captain Falcon
+30	Bayonetta
+32	Samus
+35	Marth
+\.
+
+
+--
+-- Name: characters_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
+--
+
+SELECT pg_catalog.setval('public.characters_id_seq', 55, true);
+
+
+--
+-- Name: abilities abilities_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.abilities
+    ADD CONSTRAINT abilities_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: characters characters_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.characters
+    ADD CONSTRAINT characters_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: characters unique_names; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.characters
+    ADD CONSTRAINT unique_names UNIQUE (name);
+
+
 --
 -- PostgreSQL database dump complete
 --
