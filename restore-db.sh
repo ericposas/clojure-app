c4eae7ba6220dd5d88523cc6c67d80c86b160ae7
eric <ericposas@outlook.com>
2022-07-21 13:16:21 -0500

    altered restore-db script to accept a cli argument

--- restore-db.sh
+++ restore-db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FILE=$1
+
 docker exec -it local_pgdb psql -U admin -c "CREATE DATABASE smashbros;";
 docker exec -it local_pgdb psql -U admin -c "GRANT ALL PRIVILEGES ON DATABASE smashbros TO admin;";
-cat dump_smashbros.sql | docker exec -i local_pgdb psql -U admin -d smashbros
+cat $FILE | docker exec -i local_pgdb psql -U admin -d smashbros
