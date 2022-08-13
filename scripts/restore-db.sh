4ab7d936659a9adff07ae9400d2c83870b85f121
Eric P <posas@wieck.com>
2022-08-13 13:52:30 -0500

    altered names in docker-compose file

--- scripts/restore-db.sh
+++ scripts/restore-db.sh
@@ -2,6 +2,6 @@
 
 FILE=$1
 
-docker exec -it local_pgdb psql -U admin -c "CREATE DATABASE smashbros;";
-docker exec -it local_pgdb psql -U admin -c "GRANT ALL PRIVILEGES ON DATABASE smashbros TO admin;";
-cat $FILE | docker exec -i local_pgdb psql -U admin -d smashbros
+docker exec -it local_pgdb-clojure-app psql -U admin -c "CREATE DATABASE smashbros;";
+docker exec -it local_pgdb-clojure-app psql -U admin -c "GRANT ALL PRIVILEGES ON DATABASE smashbros TO admin;";
+cat $FILE | docker exec -i local_pgdb-clojure-app psql -U admin -d smashbros
